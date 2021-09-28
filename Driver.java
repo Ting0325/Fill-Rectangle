e8aeb11421ce96ac20fdadcda6a7aa624b1d9c26
Ting0325 <41935725+Ting0325@users.noreply.github.com>
2021-09-29 07:54:45 +0800

    [update] added more obstacles

--- Driver.java
+++ Driver.java
@@ -1,18 +1,20 @@
 
 public class Driver {
 	public static void main(String[] args) {
-		Rectangle rectangle = new Rectangle(30,40);
+		Rectangle rectangle = new Rectangle(30,30);
 		rectangle.display();
 		Obstacle o0 = new Obstacle(1,2,3,5,2);
 		Obstacle o1 = new Obstacle(1,2,6,5,2);
 		Obstacle o2 = new Obstacle(1,7,11,5,2);
 		Obstacle o3 = new Obstacle(1,1,17,10,3);
 		Obstacle o4 = new Obstacle(1,15,1,5,17);
+		Obstacle o5 = new Obstacle(1,4,20,15,9);
 		rectangle.obstacles.add(o0);
 		rectangle.obstacles.add(o1);
 		rectangle.obstacles.add(o2);
 		rectangle.obstacles.add(o3);
 		rectangle.obstacles.add(o4);
+		rectangle.obstacles.add(o5);
 		rectangle.fillObstacles();
 		rectangle.display();
 		rectangle.fillFromLeft();
